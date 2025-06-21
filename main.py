b20c304064bb89df74796e75257637d5535d04cd
Fin M <fin@mace.law>
2025-06-20 22:33:44 -0400

    change getrand to getbatch

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
 
-def getrand() -> int: # gets a random number
+def getbatch() -> list[int]: # gets a random number
+    url: str = "https://www.random.org/integers/?num=100&min=1&max=6&col=1&base=10&format=plain&rnd=new"
+    
+    
     return 1 # unfinished
